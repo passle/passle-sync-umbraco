6fb63fe6a96c09b5b05cae057d10b5de9b6abded
Mark Phillips <mark@passle.net>
2022-08-23 18:12:14 +0100

    Create a new IOCComposer for the SyncHandlers

---
+++ PassleSync.Core/Composers/IOCComposer.cs
@@ -0,0 +1,17 @@
+﻿using PassleSync.Core.API.SyncHandlers;
+using PassleSync.Core.Models.Admin;
+using PassleSync.Core.SyncHandlers;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace PassleSync.Core.Composers
+{
+    public class IOCComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Register<ISyncHandler<Post>, PostHandler>(Lifetime.Request);
+            composition.Register<ISyncHandler<Person>, AuthorHandler>(Lifetime.Request);
+        }
+    }
+}
