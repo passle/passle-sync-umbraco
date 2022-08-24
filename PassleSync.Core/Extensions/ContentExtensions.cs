0b8394e5af08a9937a56383cc09a669889ce5bd7
Mark Phillips <mark@passle.net>
2022-08-24 15:43:26 +0100

    Tidy up some extensions

--- PassleSync.Core/Extensions/ContentExtensions.cs
+++ PassleSync.Core/Extensions/ContentExtensions.cs
@@ -1,34 +1,10 @@
 ﻿using System;
 using Umbraco.Core.Models;
-using Umbraco.Core.Models.PublishedContent;
 
 namespace PassleSync.Core.Extensions
 {
     public static class ContentExtensions
     {
-        public static void SetValueIfChanged(this IContent content, string propertyTypeAlias, object value, string culture = null, string segment = null)
-        {
-            var currentValue = content.GetValue(propertyTypeAlias);
-            if (Equals(currentValue, value))
-            {
-                return;
-            }
-
-            content.SetValue(propertyTypeAlias, value, culture, segment);
-        }
-
-        //public static T GetValueOrDefault<T>(this IPublishedContent content, string key, T defaultValue = default(T))
-        //{
-        //    try
-        //    {
-        //        return (T)content?.GetType()?.GetProperty(key)?.GetValue(content, null) ?? defaultValue;
-        //    }
-        //    catch (NullReferenceException ex)
-        //    {
-        //        return defaultValue;
-        //    }
-        //}
-
         public static T GetValueOrDefault<T>(this IContent content, string key, T defaultValue = default(T))
         {
             try
--- PassleSync.Core/Extensions/StringExtensions.cs
+++ PassleSync.Core/Extensions/StringExtensions.cs
@@ -1,38 +1,9 @@
 using System;
-using System.Text.RegularExpressions;
 
 namespace PassleSync.Core.Extensions
 {
     public static class StringExtensions
     {
-        public static string ToTelephoneLink(this string phoneNumber)
-        {
-            return string.Format("tel:{0}", Regex.Replace(phoneNumber, @"[^0-9]", ""));
-        }
-
-        public static string RemoveParagraphWrapperTags(this string text)
-        {
-            if (string.IsNullOrEmpty(text))
-            {
-                return text;
-            }
-
-            string trimmedText = text.Trim();
-            string upperText = trimmedText.ToUpper();
-            int paragraphIndex = upperText.IndexOf("<P>");
-
-            if (paragraphIndex != 0 ||
-                paragraphIndex != upperText.LastIndexOf("<P>") ||
-                upperText.Substring(upperText.Length - 4, 4) != "</P>")
-            {
-                // Paragraph not used as a wrapper element
-                return text;
-            }
-
-            // Remove paragraph wrapper tags
-            return trimmedText.Substring(3, trimmedText.Length - 7);
-        }
-
         public static string FirstCharToUpper(this string input)
         {
             switch (input)
