35b99c9f62b1f4d13e9e41a2c99c0c8598f4ae27
Mark Phillips <mark@passle.net>
2022-08-25 13:41:02 +0100

    Make the UI for posts and people work

--- PassleSync.Core/Extensions/StringExtensions.cs
+++ PassleSync.Core/Extensions/StringExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Web;
 
 namespace PassleSync.Core.Extensions
 {
@@ -22,6 +23,19 @@ namespace PassleSync.Core.Extensions
                 case "": throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input));
                 default: return input[0].ToString().ToLower() + input.Substring(1);
             }
+        }
+        public static string UrlEncode(this string str)
+        {
+            return HttpUtility.UrlEncode(str.Replace("'", "%27"));
+        }
+
+        public static string Truncate(this string str)
+        {
+            if (str.Length < 120)
+            {
+                return str;
+            }
+            return str.Substring(0, 120) + "...";
         }
     }      
 }
