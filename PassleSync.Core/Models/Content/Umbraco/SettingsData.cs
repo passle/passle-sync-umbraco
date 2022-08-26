3460020aa5c1a935f1dc48f723dbde2bc9cff82c
Mark Phillips <mark@passle.net>
2022-08-26 12:12:24 +0100

    Include SettingsData

---
+++ PassleSync.Core/Models/Content/Umbraco/SettingsData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PassleSync.Core.Models.Content.Umbraco
+{
+    public class SettingsData
+    {
+        public IEnumerable<string> PassleShortcodes { get; set; }
+        public string ClientApiKey { get; set; }
+        public string PluginApiKey { get; set; }
+        public string PostPermalinkPrefix { get; set; }
+        public string AuthorPermalinkPrefix { get; set; }
+        public int PostsParentNodeId { get; set; }
+        public int AuthorsParentNodeId { get; set; }
+    }
+}
