1952227d1e803d44c2c6ce029abe69036d53137e
Mark Phillips <mark@passle.net>
2022-09-08 17:10:07 +0100

    Create a default Tag page that fetches the tag from a url param

---
+++ PassleSync.Website/ViewModels/TagViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core.Models.PublishedContent;
+using Tag = PassleSync.Core.Models.Content.Umbraco.Tag;
+
+namespace PassleSync.Website.ViewModels
+{
+    public class TagViewModel : Tag
+    {
+        public TagViewModel(IPublishedContent content) : base(content) { }
+
+        public string Tag;
+        public IEnumerable<PasslePostViewModel> Posts;
+    }
+}
