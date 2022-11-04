fbfc397d813c9a0879dc2e86546bcb592f538b2f
Mark Phillips <mark@passle.net>
2022-11-04 15:17:50 +0000

    Replace the duplicated code in PostsTable and AuthorsTable with EntitiesTable

---
+++ PassleSync.Website/App_Plugins/PassleSync/backoffice/passleSync/components/organisms/entitiesTable/entitiesTable.directive.js
@@ -0,0 +1,245 @@
+﻿angular.module("umbraco.directives").directive(
+    "entitiesTable",
+    function () {
+        function EntitiesTableController($scope, notificationsService, treeService) {
+            this.isLoading = false;
+            this.isUpdating = false;
+
+            let currentSortCol = "Name";
+            let currentSortDir = "desc";
+            this.isSelectedAll = false;
+            this.syncedCount = 0;
+            this.unsyncedCount = 0;
+            this.selectedCount = 0;
+            this.entities = [];
+            this.entitiesOnShow = [];
+
+            this.pagination = {
+                pageSize: 10,
+                pageNumber: 1,
+                totalPages: 1
+            };
+
+            this.rowProperties = $scope.entityInfo.rowProperties;
+            this.entityReponseParam = $scope.entityInfo.entityReponseParam;
+            const getRowData = $scope.entityInfo.getRowData;
+            const getExisting = $scope.entityInfo.resource.getExisting;
+            const updateAll = $scope.entityInfo.resource.updateAll;
+            const syncAll = $scope.entityInfo.resource.syncAll;
+            const syncOne = $scope.entityInfo.resource.syncOne;
+            const syncMany = $scope.entityInfo.resource.syncMany;
+            const deleteAll = $scope.entityInfo.resource.deleteAll;
+            const deleteOne = $scope.entityInfo.resource.deleteOne;
+            const deleteMany = $scope.entityInfo.resource.deleteMany;
+
+            this.onload = () => {
+                this.isLoading = true;
+
+                getExisting().then((response) => {
+                    this.entities = response[this.entityReponseParam].map((entity) => getRowData(entity));
+                    this.syncedCount = this.entities.filter((entity) => entity.synced).length;
+                    this.unsyncedCount = this.entities.length - this.syncedCount;
+                    this.isSelectedAll = false;
+                    this.selectedCount = 0;
+
+                    updatePagination();
+                    updateItemsOnShow();
+
+                    this.isLoading = false;
+                }, (error) => {
+                    console.error(error);
+                    notificationsService.error("Error", error);
+                    this.isLoading = false;
+                });
+            }
+
+            this.update = () => {
+                this.isUpdating = true;
+
+                updateAll().then((response) => {
+                    this.entities = response[this.entityReponseParam].map((entity) => getRowData(entity));
+
+                    this.syncedCount = this.entities.filter((entity) => entity.synced).length;
+                    this.unsyncedCount = this.entities.length - this.syncedCount;
+                    this.isSelectedAll = false;
+                    this.selectedCount = 0;
+
+                    updatePagination();
+                    updateItemsOnShow();
+
+                    this.isUpdating = false;
+
+                    syncTree();
+                }, (error) => {
+                    console.error(error);
+                    notificationsService.error("Error", error);
+
+                    this.isUpdating = false;
+                });
+            }
+
+            this.sync = () => {
+                this.isUpdating = true;
+
+                let syncProm;
+                let shortcodes = [];
+
+                if (this.selectedCount == 0) {
+                    syncProm = syncAll();
+                } else if (this.selectedCount == 1) {
+                    shortcodes = this.entities.filter((entity) => entity.selected).map((entity) => entity.shortcode);
+                    syncProm = syncOne(shortcodes);
+                } else {
+                    shortcodes = this.entities.filter((entity) => entity.selected).map((entity) => entity.shortcode);
+                    syncProm = syncMany(shortcodes);
+                }
+
+                syncProm.then((response) => {
+                    this.entities.forEach((entity, ii) => {
+                        let matchingEntities = response[this.entityReponseParam].filter(x => x.Shortcode === entity.shortcode);
+                        if (matchingEntities.length > 0) {
+                            this.entities[ii] = getRowData(matchingEntities[0]);
+                        }
+                    });
+
+                    this.syncedCount = this.entities.filter((entity) => entity.synced).length;
+                    this.unsyncedCount = this.entities.length - this.syncedCount;
+                    this.isSelectedAll = false;
+                    this.selectedCount = 0;
+
+                    updatePagination();
+                    updateItemsOnShow();
+
+                    this.isUpdating = false;
+
+                    notificationsService.success("Success", $scope.entityInfo.namePlural + " have been synced");
+
+                    syncTree();
+                }, (error) => {
+                    console.error(error);
+                    notificationsService.error("Error", error);
+
+                    this.isUpdating = false;
+                });
+            }
+
+            this.delete = () => {
+                this.isUpdating = true;
+
+                let deleteProm;
+                let shortcodes = [];
+                if (this.selectedCount == 0) {
+                    deleteProm = deleteAll();
+                } else if (this.selectedCount == 1) {
+                    shortcodes = this.entities.filter((entity) => entity.selected).map((entity) => entity.shortcode);
+                    deleteProm = deleteOne(shortcodes);
+                } else {
+                    shortcodes = this.entities.filter((entity) => entity.selected).map((entity) => entity.shortcode);
+                    deleteProm = deleteMany(shortcodes);
+                }
+
+                deleteProm.then((response) => {
+                    this.entities.forEach((entity, ii) => {
+                        let matchingEntities = response[this.entityReponseParam].filter(x => x.Shortcode === entity.shortcode);
+                        if (matchingEntities.length > 0) {
+                            this.entities[ii] = getRowData(matchingEntities[0], true);
+                        }
+                    });
+                    this.syncedCount = this.entities.filter((entity) => entity.synced).length;
+                    this.unsyncedCount = this.entities.length - this.syncedCount;
+                    this.isSelectedAll = false;
+                    this.selectedCount = 0;
+
+                    updatePagination();
+                    updateItemsOnShow();
+
+                    this.isUpdating = false;
+
+                    notificationsService.success("Success", $scope.entityInfo.namePlural + " have been deleted");
+
+                    syncTree();
+                }, (error) => {
+                    console.error(error);
+                    notificationsService.error("Error", error);
+
+                    this.isUpdating = false;
+                });
+            }
+
+            this.allowSelectAll = true;
+            this.onSelectItem = (selectedItem) => {
+                selectedItem.selected = !selectedItem.selected;
+                this.selectedCount = this.entities.filter((entity) => entity.selected).length;
+            }
+            this.onSelectAll = () => {
+                this.isSelectedAll = !this.isSelectedAll;
+                this.entitiesOnShow.forEach((entity) => entity.selected = this.isSelectedAll);
+                this.selectedCount = this.isSelectedAll ? this.entities.length : 0;
+            }
+            this.onSelectedAll = () => {
+                return this.isSelectedAll;
+            };
+            this.onSortDirection = (col, direction) => {
+                return col === currentSortCol && direction === currentSortDir;
+            }
+            this.onSort = (field, allow) => {
+                if (allow) {
+                    if (field === currentSortCol) {
+                        if (currentSortDir === "asc") {
+                            currentSortDir = "desc";
+                        } else {
+                            currentSortDir = "asc";
+                        }
+                    } else {
+                        currentSortCol = field;
+                        currentSortDir = "desc";
+                    }
+
+                    this.entities.sort((a, b) => (a[currentSortCol] > b[currentSortCol] && currentSortDir === "desc") ? 1 : -1);
+                }
+            }
+
+            this.nextPage = () => this.goToPage(this.pagination.pageNumber + 1);
+            this.prevPage = () => this.goToPage(this.pagination.pageNumber - 1);
+            this.goToPage = (pageNo) => {
+                this.pagination = {
+                    ...this.pagination,
+                    pageNumber: pageNo,
+                };
+
+                updateItemsOnShow();
+            }
+            const updatePagination = () => {
+                this.pagination = {
+                    ...this.pagination,
+                    pageNumber: 1,
+                    totalPages: Math.ceil(this.entities.length / this.pagination.pageSize)
+                };
+            }
+
+            const syncTree = () => treeService.clearCache({ section: "content" });
+
+            const updateItemsOnShow = () => {
+                this.entitiesOnShow = this.entities.slice(
+                    (this.pagination.pageNumber - 1) * this.pagination.pageSize,
+                    Math.min(this.pagination.pageNumber * this.pagination.pageSize, this.entities.length)
+                );
+            }
+        };
+
+        function link(scope, element, attrs, controller) {
+            controller.onload();
+        }
+
+        return {
+            restrict: 'E',
+            templateUrl: '/App_Plugins/PassleSync/backoffice/passleSync/components/organisms/entitiesTable/entitiesTable.html',
+            scope: {
+                entityInfo: '='
+            },
+            controller: ['$scope', 'notificationsService', 'treeService', EntitiesTableController],
+            controllerAs: 'vm',
+            link: link
+        }
+    }
+);
--- PassleSync.Website/App_Plugins/PassleSync/backoffice/passleSync/components/organisms/postsTable/postsTable.controller.js
+++ PassleSync.Website/App_Plugins/PassleSync/backoffice/passleSync/components/organisms/postsTable/postsTable.controller.js
@@ -1,270 +1,31 @@
 ﻿angular.module("umbraco").controller(
     "PostsTableController",
-    function (notificationsService, treeService, passlePostsResource) {
+    function (passlePostsResource) {
         var vm = this;
-        vm.isLoading = false;
-        vm.isUpdating = false;
 
-        let currentSortCol = "Name";
-        let currentSortDir = "desc";
-        vm.isSelectedAll = false;
-        vm.syncedCount = 0;
-        vm.unsyncedCount = 0;
-        vm.selectedCount = 0;
-        vm.posts = [];
-        vm.postsOnShow = [];
+        vm.entityInfo = {
+            nameSingle: "Post",
+            namePlural: "Posts",
+            getRowData: function getRowData(post, deletedOverride = false) {
+                // Convert the returned model to the format the table needs
+                // The override is used when deleting data:
+                // - we return the last state of the content before it was deleted, so we need to override the synced and url values
 
-        vm.pagination = {
-            pageSize: 10,
-            pageNumber: 1,
-            totalPages: 1
-        };
-
-        function getPostDataObject(post, deletedOverride = false) {
-            // Convert the returned model to the format the table needs
-            // The override is used when deleting data:
-            // - we return the last state of the content before it was deleted, so we need to override the synced and url values
-
-            return Object.assign({}, {
-                "name": post.Title,
-                "excerpt": post.Excerpt,
-                "editPath": (post.Synced && !deletedOverride) ? ("/content/content/edit/" + post.Id) : post.PostUrl,
-                "shortcode": post.Shortcode,
-                "synced": post.Synced && !deletedOverride
-            });
-        }
-        function getOverriddenPostDataObject(post) {
-            return getPostDataObject(post, true);
-        }
-
-        function syncTree() {
-            treeService.clearCache({ section: "content" });
-        }
-
-        function UpdatePagination() {
-            vm.pagination = {
-                ...vm.pagination,
-                pageNumber: 1,
-                totalPages: Math.ceil(vm.posts.length / vm.pagination.pageSize)
-            };
-        }
-
-        function UpdateItemsOnShow() {
-            vm.postsOnShow = vm.posts.slice(
-                (vm.pagination.pageNumber - 1) * vm.pagination.pageSize,
-                Math.min(vm.pagination.pageNumber * vm.pagination.pageSize, vm.posts.length)
-            );
-        }
-
-        function onload() {
-            vm.isLoading = true;
-
-            passlePostsResource.getExisting().then((response) => {
-                vm.posts = response.Posts.map((post) => getPostDataObject(post));
-                vm.syncedCount = vm.posts.filter((post) => post.synced).length;
-                vm.unsyncedCount = vm.posts.length - vm.syncedCount;
-                vm.isSelectedAll = false;
-                vm.selectedCount = 0;
-
-                UpdatePagination();
-                UpdateItemsOnShow();
-
-                vm.isLoading = false;
-            }, (error) => {
-                console.error(error);
-                notificationsService.error("Error", error);
-                vm.isLoading = false;
-            });
-        }
-        onload();
-
-        vm.update = function () {
-            vm.isUpdating = true;
-
-            passlePostsResource.updateAll().then((response) => {
-                vm.posts = response.Posts.map((post) => getPostDataObject(post));
-
-                vm.syncedCount = vm.posts.filter((post) => post.synced).length;
-                vm.unsyncedCount = vm.posts.length - vm.syncedCount;
-                vm.isSelectedAll = false;
-                vm.selectedCount = 0;
-
-                UpdatePagination();
-                UpdateItemsOnShow();
-
-                vm.isUpdating = false;
-
-                syncTree();
-            }, (error) => {
-                console.error(error);
-                notificationsService.error("Error", error);
-
-                vm.isUpdating = false;
-            });
-        }
-
-        vm.sync = function () {
-            vm.isUpdating = true;
-
-            let syncProm;
-            let shortcodes = [];
-
-            if (vm.selectedCount == 0) {
-                syncProm = passlePostsResource.syncAll();
-            } else if (vm.selectedCount == 1) {
-                shortcodes = vm.posts.filter((post) => post.selected).map((post) => post.shortcode);
-                syncProm = passlePostsResource.syncOne(shortcodes);
-            } else {
-                shortcodes = vm.posts.filter((post) => post.selected).map((post) => post.shortcode);
-                syncProm = passlePostsResource.syncMany(shortcodes);
-            }
-
-            syncProm.then((response) => {
-                vm.posts.forEach((post, ii) => {
-                    let matchingPosts = response.Posts.filter(x => x.Shortcode === post.shortcode);
-                    if (matchingPosts.length > 0) {
-                        vm.posts[ii] = getPostDataObject(matchingPosts[0]);
-                    }
+                return Object.assign({}, {
+                    "name": post.Title,
+                    "excerpt": post.Excerpt,
+                    "editPath": (post.Synced && !deletedOverride) ? ("/content/content/edit/" + post.Id) : post.PostUrl,
+                    "shortcode": post.Shortcode,
+                    "synced": post.Synced && !deletedOverride
                 });
-
-                vm.syncedCount = vm.posts.filter((post) => post.synced).length;
-                vm.unsyncedCount = vm.posts.length - vm.syncedCount;
-                vm.isSelectedAll = false;
-                vm.selectedCount = 0;
-
-                UpdatePagination();
-                UpdateItemsOnShow();
-
-                vm.isUpdating = false;
-
-                notificationsService.success("Success", "Posts have been synced");
-
-                syncTree();
-            }, (error) => {
-                console.error(error);
-                notificationsService.error("Error", error);
-
-                vm.isUpdating = false;
-            });
-        }
-
-        vm.delete = function () {
-            vm.isUpdating = true;
-
-            let deleteProm;
-            let shortcodes = [];
-            if (vm.selectedCount == 0) {
-                deleteProm = passlePostsResource.deleteAll();
-            } else if (vm.selectedCount == 1) {
-                shortcodes = vm.posts.filter((post) => post.selected).map((post) => post.shortcode);
-                deleteProm = passlePostsResource.deleteOne(shortcodes);
-            } else {
-                shortcodes = vm.posts.filter((post) => post.selected).map((post) => post.shortcode);
-                deleteProm = passlePostsResource.deleteMany(shortcodes);
-            }
-
-            deleteProm.then((response) => {
-                vm.posts.forEach((post, ii) => {
-                    let matchingPosts = response.Posts.filter(x => x.Shortcode === post.shortcode);
-                    if (matchingPosts.length > 0) {
-                        vm.posts[ii] = getOverriddenPostDataObject(matchingPosts[0]);
-                    }
-                });
-                vm.syncedCount = vm.posts.filter((post) => post.synced).length;
-                vm.unsyncedCount = vm.posts.length - vm.syncedCount;
-                vm.isSelectedAll = false;
-                vm.selectedCount = 0;
-
-                UpdatePagination();
-                UpdateItemsOnShow();
-
-                vm.isUpdating = false;
-
-                notificationsService.success("Success", "Posts have been deleted");
-
-                syncTree();
-            }, (error) => {
-                console.error(error);
-                notificationsService.error("Error", error);
-
-                vm.isUpdating = false;
-            });
-        }
-
-
-        vm.posts = [];
-        vm.rowProperties = [
-            { alias: "excerpt", header: "Excerpt", allowSorting: false },
-            { alias: "synced", header: "Synced?", allowSorting: true }
-        ];
-        vm.allowSelectAll = true;
-        vm.onSelectItem = function (selectedItem) {
-            selectedItem.selected = !selectedItem.selected;
-            vm.selectedCount = vm.posts.filter((post) => post.selected).length;
-        }
-        vm.onSelectAll = function () {
-            vm.isSelectedAll = !vm.isSelectedAll;
-            vm.postsOnShow.forEach((post) => post.selected = vm.isSelectedAll);
-            vm.selectedCount = vm.isSelectedAll ? vm.posts.length : 0;
-        }
-        vm.onSelectedAll = function () {
-            return vm.isSelectedAll;
+            },
+            resource: passlePostsResource,
+            entityReponseParam: 'Posts',
+            rowProperties: [
+                { alias: "excerpt", header: "Excerpt", allowSorting: false },
+                { alias: "synced", header: "Synced?", allowSorting: true }
+            ]
         };
-        vm.onSortDirection = function (col, direction) {
-            return col === currentSortCol && direction === currentSortDir;
-        }
-        vm.onSort = function (field, allow) {
-            if (allow) {
-                if (field === currentSortCol) {
-                    if (currentSortDir === "asc") {
-                        currentSortDir = "desc";
-                    } else {
-                        currentSortDir = "asc";
-                    }
-                } else {
-                    currentSortCol = field;
-                    currentSortDir = "desc";
-                }
-
-                vm.posts.sort((a, b) => (a[currentSortCol] > b[currentSortCol] && currentSortDir === "desc") ? 1 : -1);
-            }
-        }
-
-        vm.nextPage = function () {
-            vm.pagination = {
-                ...vm.pagination,
-                pageNumber: vm.pagination.pageNumber + 1,
-            };
-
-            UpdateItemsOnShow();
-        }
-
-        vm.prevPage = function () {
-            vm.pagination = {
-                ...vm.pagination,
-                pageNumber: vm.pagination.pageNumber - 1,
-            };
-
-            UpdateItemsOnShow();
-        }
-
-        vm.changePage = function (pageNo) {
-            vm.pagination = {
-                ...vm.pagination,
-                pageNumber: pageNo,
-            };
-
-            UpdateItemsOnShow();
-        }
-
-        vm.goToPage = function (pageNo) {
-            vm.pagination = {
-                ...vm.pagination,
-                pageNumber: pageNo,
-            };
-
-            UpdateItemsOnShow();
-        }
     }
 );
 
