8e91284228acd9a39f324ffb023c5239922cd5e1
Mark Phillips <mark@passle.net>
2022-11-21 15:41:00 +0000

    Actually commit the boolean property editor

---
+++ PassleSync.Core/PropertyEditors/PassleBooleanPropertyEditor.cs
@@ -0,0 +1,20 @@
+﻿using Umbraco.Core.Logging;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Web.PropertyEditors;
+
+namespace PassleSync.Core.PropertyEditors
+{
+    [DataEditor("Passle Boolean", EditorType.PropertyValue | EditorType.MacroParameter, "Passle Boolean", "~/App_Plugins/PassleSync/propertyEditors/boolean/boolean.html", ValueType = "INT", Group = "Passle", Icon = "icon-checkbox")]
+    public class PassleBooleanPropertyEditor : DataEditor
+    {
+        public PassleBooleanPropertyEditor(ILogger logger)
+            : base(logger)
+        {
+        }
+
+        protected override IConfigurationEditor CreateConfigurationEditor()
+        {
+            return new TrueFalseConfigurationEditor();
+        }
+    }
+}
