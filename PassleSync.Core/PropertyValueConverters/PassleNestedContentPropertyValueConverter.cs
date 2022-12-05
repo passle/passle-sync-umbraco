53167febeab8c194166639e26e6cae32ec1dddfe
Mark Phillips <mark@passle.net>
2022-12-05 10:21:44 +0000

    Got it working by inheriting from Umbraco's NestedContentManyValueConverter

---
+++ PassleSync.Core/PropertyValueConverters/PassleNestedContentPropertyValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web.PropertyEditors.ValueConverters;
+using Umbraco.Web.PublishedCache;
+
+namespace PassleSync.Core.PropertyValueConverters
+{
+    public class PassleNestedContentPropertyValueConverter : NestedContentManyValueConverter
+    {
+        public PassleNestedContentPropertyValueConverter(
+            IPublishedSnapshotAccessor publishedSnapshotAccessor,
+            IPublishedModelFactory publishedModelFactory,
+            IProfilingLogger proflog
+        ) : base(publishedSnapshotAccessor, publishedModelFactory, proflog)
+        { }
+
+        public override bool IsConverter(IPublishedPropertyType propertyType)
+        {
+            return propertyType.EditorAlias.InvariantEquals("PassleSync.NestedContent");
+        }
+    }
+}
