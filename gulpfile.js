e2801fa298626702b80f9c98e39a58f83e21d13d
Felix Packard <felix@passle.net>
2023-05-02 11:12:12 +0100

    Fix packaging issues after merge

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,34 @@
 const { src, dest, series } = require("gulp");
 const replace = require("gulp-replace");
 const rename = require("gulp-rename");
+const watch = require("gulp-watch");
+const cleanDest = require("gulp-clean-dest");
 const fs = require("fs");
 const { spawn } = require("child_process");
 const packageJson = require("./package.json");
 
+const paths = {
+  source: "./PassleSync.Core/App_Plugins",
+  dest: "./PassleSync.Website/App_Plugins",
+};
+
+const copyPluginFiles = (cb) => {
+  return src(paths.source + "/**/*", { base: paths.source })
+    .pipe(cleanDest(paths.dest))
+    .pipe(dest(paths.dest));
+}
+
+const watchAndCopyPluginFiles = (cb) => {
+  return src(paths.source + "/**/*", { base: paths.source })
+    .pipe(watch(paths.source, { base: paths.source }))
+    .pipe(cleanDest(paths.dest))
+    .pipe(dest(paths.dest));
+}
+
 const getVersion = () => packageJson.version;
 
 const updatePackageXML = (cb) => {
-  const xml = src("package_template.xml")
+  src("package_template.xml")
     .pipe(replace("PASSLESYNC_VERSION", getVersion()))
     .pipe(replace("HOMEPAGE_TEMPLATE", fs.readFileSync("PassleSync.Website/Views/HomePage.cshtml", "utf-8")))
     .pipe(replace("INSIGHTSPAGE_TEMPLATE", fs.readFileSync("PassleSync.Website/Views/InsightsPage.cshtml", "utf-8")))
@@ -30,5 +50,7 @@ const runUmbPack = (cb) => {
   }).on("close", cb);
 };
 
-exports.package = updatePackageXML;
-exports.packageAndZip = series(updatePackageXML, runUmbPack);
+exports.copyPluginFiles = copyPluginFiles;
+exports.watch = watchAndCopyPluginFiles;
+exports.package = series(copyPluginFiles, updatePackageXML);
+exports.packageAndZip = series(copyPluginFiles, updatePackageXML, runUmbPack);
